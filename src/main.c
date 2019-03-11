8c815374f077895b256ebaa0976f9e84e3808d61
Daniel Fekete <hello@danfekete.com>
2019-03-11 10:47:15 +0100

    Test draw

--- src/main.c
+++ src/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 #include "bmp.h"
 #include "mono.h"
 #include "rle.h"
 #include "h_writer.h"
+#include "draw.h"
 #include "../argparse/argparse.h"
 
 static const char *const usage[] = {
@@ -10,17 +12,41 @@ static const char *const usage[] = {
     NULL,
 };
 
+const uint16_t width = 128;
+const uint16_t height = 64;
+unsigned char *displayBuffer;
+
+void draw(uint16_t x, uint16_t y, uint32_t color)
+{
+    displayBuffer[x + width * y] = color > 0x00 ? 'x' : ' ';
+}
+
+void outputDisplayBuffer()
+{
+    printf("\n");
+    for(int i=0;i<width*height;i++)
+    {
+        printf("%c", displayBuffer[i]);
+        if(i % width == 0 && i > 0) printf("\n");
+    }
+    printf("\n");
+}
+
 int main(int argc, char **argv) 
 {
     float dither = 0.5;
     const char *path = NULL;
     const unsigned char floyd;
+    const unsigned char test;
+
+    uint32_t imageWidth, imageHeight;
 
     struct argparse_option options[] = {
         OPT_HELP(),
         OPT_STRING('o', "output", &path, "Output"),
         OPT_FLOAT('d', "dither", &dither, "Dither threshold"),
         OPT_BOOLEAN('f', 'floyd', &floyd, "Use Floyd-Steinberg algorithm to dither"),
+        OPT_BOOLEAN('t', 'test', &test, 'Test the dithered image'),
         OPT_END()
     };
 
@@ -37,6 +63,9 @@ int main(int argc, char **argv)
     MONOBUFFER *mono = (MONOBUFFER*)malloc(sizeof(MONOBUFFER));
     RLE *rle = (RLE*)malloc(sizeof(RLE));
     
+    imageWidth = buf->width;
+    imageHeight = buf->height;
+
     BUFFER_Grayscale(buf);
     MONO_Init(mono, buf->width, buf->height);
     MONO_FromBuffer(mono, buf);
@@ -66,6 +95,17 @@ int main(int argc, char **argv)
     }
     
     HEADER_Writer(rle->buffer, rle->size, f);
+
+    if(test) 
+    {
+        displayBuffer = (uint8_t*)malloc(sizeof(uint8_t)*width*height);
+        memset(displayBuffer, ' ', width*height);
+        DRAW* d = (DRAW*)malloc(sizeof(DRAW));
+        DRAW_Init(d, &draw);
+        DRAW_CompressedBitmap(d, rle->buffer, rle->size, imageWidth, imageHeight);
+        outputDisplayBuffer();
+        free(displayBuffer);
+    }
 
     RLE_Deinit(rle); // Kill RLE
     return 0;
